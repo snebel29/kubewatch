cae80520de4fc9a2b8b53f78b215fe6cd0d3a96a
Sven Nebel <snebel29@users.noreply.github.com>
2018-10-12 18:09:28 +0100

    Feat/new configuration (#9)
    * Clean/Simplify config logic by using viper

--- config/config.go
+++ config/config.go
@@ -16,210 +16,59 @@ limitations under the License.
 
 package config
 
-import (
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"runtime"
-
-	"gopkg.in/yaml.v2"
-)
-
-var ConfigFileName = ".kubewatch.yaml"
+type Config struct {
+	Handler   Handler  `mapstructure:"handler"`
+	Resource  Resource `mapstructure:"resource"`
+	Namespace string   `mapstructure:"namespace,omitempty"`
+	// for watching specific namespace, leave it empty for watching all.
+	// this config is ignored when watching namespaces
+}
 
 type Handler struct {
-	Slack      Slack      `json:"slack"`
-	Hipchat    Hipchat    `json:"hipchat"`
-	Mattermost Mattermost `json:"mattermost"`
-	Flock      Flock      `json:"flock"`
-	Webhook    Webhook    `json:"webhook"`
+	Slack      Slack      `mapstructure:"slack"`
+	Hipchat    Hipchat    `mapstructure:"hipchat"`
+	Mattermost Mattermost `mapstructure:"mattermost"`
+	Flock      Flock      `mapstructure:"flock"`
+	Webhook    Webhook    `mapstructure:"webhook"`
 }
 
 // Resource contains resource configuration
 type Resource struct {
-	Deployment            bool `json:"deployment"`
-	ReplicationController bool `json:"rc"`
-	ReplicaSet            bool `json:"rs"`
-	DaemonSet             bool `json:"ds"`
-	Services              bool `json:"svc"`
-	Pod                   bool `json:"po"`
-	Job                   bool `json:"job"`
-	PersistentVolume      bool `json:"pv"`
-	Namespace             bool `json:"ns"`
-	Secret                bool `json:"secret"`
-	ConfigMap             bool `json:"configmap"`
-	Ingress               bool `json:"ing"`
+	Deployment            bool `mapstructure:"deployment"`
+	ReplicationController bool `mapstructure:"rc"`
+	ReplicaSet            bool `mapstructure:"rs"`
+	DaemonSet             bool `mapstructure:"ds"`
+	Services              bool `mapstructure:"svc"`
+	Pod                   bool `mapstructure:"po"`
+	Job                   bool `mapstructure:"job"`
+	PersistentVolume      bool `mapstructure:"pv"`
+	Namespace             bool `mapstructure:"ns"`
+	Secret                bool `mapstructure:"secret"`
+	ConfigMap             bool `mapstructure:"configmap"`
+	Ingress               bool `mapstructure:"ing"`
 }
 
-// Config struct contains kubewatch configuration
-type Config struct {
-	Handler Handler `json:"handler"`
-	//Reason   []string `json:"reason"`
-	Resource Resource `json:"resource"`
-	// for watching specific namespace, leave it empty for watching all.
-	// this config is ignored when watching namespaces
-	Namespace string `json:"namespace,omitempty"`
-}
-
-// Slack contains slack configuration
 type Slack struct {
-	Token   string `json:"token"`
-	Channel string `json:"channel"`
+	Token   string `mapstructure:"token"`
+	Channel string `mapstructure:"channel"`
 }
 
-// Hipchat contains hipchat configuration
 type Hipchat struct {
-	Token string `json:"token"`
-	Room  string `json:"room"`
-	Url   string `json:"url"`
+	Token string `mapstructure:"token"`
+	Room  string `mapstructure:"room"`
+	Url   string `mapstructure:"url"`
 }
 
-// Mattermost contains mattermost configuration
 type Mattermost struct {
-	Channel  string `json:"room"`
-	Url      string `json:"url"`
-	Username string `json:"username"`
+	Channel  string `mapstructure:"room"`
+	Url      string `mapstructure:"url"`
+	Username string `mapstructure:"username"`
 }
 
-// Flock contains flock configuration
 type Flock struct {
-	Url string `json:"url"`
+	Url string `mapstructure:"url"`
 }
 
-// Webhook contains webhook configuration
 type Webhook struct {
-	Url string `json:"url"`
-}
-
-// New creates new config object
-func New() (*Config, error) {
-	c := &Config{}
-	if err := c.Load(); err != nil {
-		return c, err
-	}
-
-	return c, nil
-}
-
-func createIfNotExist() error {
-	// create file if not exist
-	configFile := filepath.Join(configDir(), ConfigFileName)
-	_, err := os.Stat(configFile)
-	if err != nil {
-		if os.IsNotExist(err) {
-			file, err := os.Create(configFile)
-			if err != nil {
-				return err
-			}
-			file.Close()
-		} else {
-			return err
-		}
-	}
-	return nil
-}
-
-// Load loads configuration from config file
-func (c *Config) Load() error {
-	err := createIfNotExist()
-	if err != nil {
-		return err
-	}
-
-	file, err := os.Open(getConfigFile())
-	if err != nil {
-		return err
-	}
-
-	b, err := ioutil.ReadAll(file)
-	if err != nil {
-		return err
-	}
-
-	if len(b) != 0 {
-		return yaml.Unmarshal(b, c)
-	}
-
-	return nil
-}
-
-func (c *Config) CheckMissingResourceEnvvars() {
-	if !c.Resource.DaemonSet && os.Getenv("KW_DAEMONSET") == "true" {
-		c.Resource.DaemonSet = true
-	}
-	if !c.Resource.ReplicaSet && os.Getenv("KW_REPLICASET") == "true" {
-		c.Resource.ReplicaSet = true
-	}
-	if !c.Resource.Namespace && os.Getenv("KW_NAMESPACE") == "true" {
-		c.Resource.Namespace = true
-	}
-	if !c.Resource.Deployment && os.Getenv("KW_DEPLOYMENT") == "true" {
-		c.Resource.Deployment = true
-	}
-	if !c.Resource.Pod && os.Getenv("KW_POD") == "true" {
-		c.Resource.Pod = true
-	}
-	if !c.Resource.ReplicationController && os.Getenv("KW_REPLICATION_CONTROLLER") == "true" {
-		c.Resource.ReplicationController = true
-	}
-	if !c.Resource.Services && os.Getenv("KW_SERVICE") == "true" {
-		c.Resource.Services = true
-	}
-	if !c.Resource.Job && os.Getenv("KW_JOB") == "true" {
-		c.Resource.Job = true
-	}
-	if !c.Resource.PersistentVolume && os.Getenv("KW_PERSISTENT_VOLUME") == "true" {
-		c.Resource.PersistentVolume = true
-	}
-	if !c.Resource.Secret && os.Getenv("KW_SECRET") == "true" {
-		c.Resource.Secret = true
-	}
-	if !c.Resource.ConfigMap && os.Getenv("KW_CONFIGMAP") == "true" {
-		c.Resource.ConfigMap = true
-	}
-	if !c.Resource.Ingress && os.Getenv("KW_INGRESS") == "true" {
-		c.Resource.Ingress = true
-	}
-	if (c.Handler.Slack.Channel == "") && (os.Getenv("SLACK_CHANNEL") != "") {
-		c.Handler.Slack.Channel = os.Getenv("SLACK_CHANNEL")
-	}
-	if (c.Handler.Slack.Token == "") && (os.Getenv("SLACK_TOKEN") != "") {
-		c.Handler.Slack.Token = os.Getenv("SLACK_TOKEN")
-	}
-}
-
-func (c *Config) Write() error {
-	b, err := yaml.Marshal(c)
-	if err != nil {
-		return err
-	}
-
-	err = ioutil.WriteFile(getConfigFile(), b, 0644)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func getConfigFile() string {
-	configFile := filepath.Join(configDir(), ConfigFileName)
-	if _, err := os.Stat(configFile); err == nil {
-		return configFile
-	}
-
-	return ""
-}
-
-func configDir() string {
-	if runtime.GOOS == "windows" {
-		home := os.Getenv("USERPROFILE")
-		return home
-	}
-	return os.Getenv("HOME")
-	//path := "/etc/kubewatch"
-	//if _, err := os.Stat(path); os.IsNotExist(err) {
-	//	os.Mkdir(path, 755)
-	//}
-	//return path
+	Url string `mapstructure:"url"`
 }
